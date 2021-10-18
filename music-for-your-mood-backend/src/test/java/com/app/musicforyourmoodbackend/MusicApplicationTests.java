49aebd5253df25242e420771e61539aad3420c3e
Mihai Bogdan <andreimihai99@yahoo.com>
2021-10-18 19:16:43 +0300

    initialized backend

---
+++ music-for-your-mood-backend/src/test/java/com/app/musicforyourmoodbackend/MusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.musicforyourmoodbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
